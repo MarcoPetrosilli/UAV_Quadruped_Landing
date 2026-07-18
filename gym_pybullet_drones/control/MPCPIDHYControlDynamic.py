important fix about the feedforward term, it has always been given 0, now fixed. and in MPC computation is considered the evolution of the final wp over the N-horizon, before it was kept fixed by mistake

gym_pybullet_drones/control/MPCPIDHYControlDynamic.py
@@ -106,17 +106,18 @@ class MPCPIDHYControlDynamic(BaseControl):
         cur_pos = np.array(cur_pos)
         cur_vel = np.array(cur_vel)
         target_pos = np.array(target_pos)
+        target_v = np.array(target_vel, dtype=float)
 
         
         
-        tf = np.linalg.norm(cur_vel-target_vel)/self.a_reach_xy
-        d_xy_stop = np.linalg.norm(target_pos[0:2] + target_vel[0:2]*tf - cur_pos[0:2])
+        tf = np.linalg.norm(cur_vel-target_v)/self.a_reach_xy
+        d_xy_stop = np.linalg.norm(target_pos[0:2] + target_v[0:2]*tf - cur_pos[0:2])
         
-        d_xy_reach = np.linalg.norm((target_pos[0:2] + target_vel[0:2]*self.N*self.dt) - cur_pos[0:2])
+        d_xy_reach = np.linalg.norm((target_pos[0:2] + target_v[0:2]*self.N*self.dt) - cur_pos[0:2])
         
         v_xy = np.linalg.norm(cur_vel[0:2])
         
-        v_target = np.linalg.norm(target_vel[0:2])
+        v_target = np.linalg.norm(target_v[0:2])
         
         stop_dist_xy = (v_xy ** 2 - v_target ** 2) / (2.0 * self.a_reach_xy + 1e-9)
         arrestable_xy = (stop_dist_xy <= self.reach_margin * max(d_xy_stop, 1e-6)) or d_xy_stop < 0.05
@@ -222,6 +223,7 @@ class MPCPIDHYControlDynamic(BaseControl):
         cost = 0
         cons = [x[:, 0] == [cur_x, cur_y, cur_vx, cur_vy]]
         for k in range(self.N):
+            xref = np.array([wp[0]+target_vel[0]*k*self.dt, wp[1]+target_vel[1]*k*self.dt, target_vel[0], target_vel[1]])
             cost += cp.quad_form(x[:, k] - xref, self.Q_hrz)
             cost += cp.quad_form(u[:, k], self.R_hrz)
             cons += [x[:, k + 1] == self.A_hrz @ x[:, k] + self.B_hrz @ u[:, k]]

gym_pybullet_drones/examples/thesis/MPC_PID_hybrid_dynamic.py
@@ -52,7 +52,7 @@ def initialize_state(num_drones, states, wp_counters):
 def update_state(pos_e, num_drones, states, wp_counters, old_wp_id, stop_delta):
     for j in range(num_drones):
         distance = np.linalg.norm(pos_e[j])
-        if distance < stop_delta:
+        if distance <= stop_delta:
             if states[j] == "rising":
                 states[j] = "nav_to_wp"
                 old_wp_id = 1
@@ -150,7 +150,7 @@ def run(drone=DEFAULT_DRONES,
     ####################################################################
     #### Waypoints #####################################################
     ####################################################################
-    v_plat = [0.1, 0.0, 0.0]
+    v_plat = [0.3, 0.0, 0.0]
     w_plat = [0.0, 0.0, 0.0]
 
     WP_MISSION = np.array([
@@ -252,7 +252,7 @@ def run(drone=DEFAULT_DRONES,
                     state=obs[j],
                     target_pos=p_LOS,
                     target_rpy=INIT_RPYS[j, :],
-                    target_vel=np.zeros(3),          # zero velocity reference
+                    target_vel=target_v,    
                     target_rpy_rates=np.zeros(3),
                     a_xy_lim=a_xy,
                     final_pos=WP_MISSION[3],
@@ -260,12 +260,14 @@ def run(drone=DEFAULT_DRONES,
 
             pos_e[j] = WP_MISSION[wp_counters[j]] - obs[j][0:3]
 
-            if old_wp_id == 3 and wp_counters[j] == 0:
-                WP_MISSION[0] = WP_MISSION[3]
-                action[j, :] = np.zeros(4)
+            
 
         [states, wp_counters, old_wp_id, stop_delta] = update_state(
             pos_e, num_drones, states, wp_counters, old_wp_id, stop_delta)
+            
+        if old_wp_id == 3 and wp_counters[j] == 0:
+                WP_MISSION[0] = WP_MISSION[3]
+                action[j, :] = np.zeros(4)
 
         #### Log #######################################################
         for j in range(num_drones):